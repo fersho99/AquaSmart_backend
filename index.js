9c630e157500689df27c076e978c251ced6baaae
fersho99 <bryanmedzav@gmail.com>
2025-07-27 00:42:35 -0700

    Agrega un prueba para ver estatus

--- index.js
+++ index.js
@@ -8,13 +8,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Ruta raíz para probar que el servidor está vivo
+app.get('/', (req, res) => {
+  res.send('¡Backend AquaSmart funcionando!');
+});
+
 // Rutas
 app.use('/auth', require('./routes/auth'));
 app.use('/peces', require('./routes/peces'));
 app.use('/lecturas', require('./routes/lecturas'));
 app.use('/alimentacion', require('./routes/alimentacion'));
 
-// Puerto dinámico para Render o 3000 en local
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
